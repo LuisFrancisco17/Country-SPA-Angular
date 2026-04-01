ec5348cf8b466e4b37a6d6af757f33d788ddf671
Luis Gil <luisfrancisco17msn@gmail.com>
2026-04-01 10:15:14 -0500

    Implementación de interfaces y mapeo de datos

--- src/app/country/page/by-capital-page/by-capital-page.ts
+++ src/app/country/page/by-capital-page/by-capital-page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInput } from '../../components/search-input/search-input';
 import { CountryList } from '../../components/country-list/country-list';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -8,7 +10,23 @@ import { CountryList } from '../../components/country-list/country-list';
   templateUrl: './by-capital-page.html',
 })
 export class ByCapitalPage {
-  onSearch(value: string) {
-    console.log({ value });
+  countryService = inject(CountryService);
+
+  isLoading = signal(false);
+  isError = signal<string | null>(null);
+  countries = signal<Country[]>([]);
+
+  onSearch(query: string) {
+    if (this.isLoading()) return;
+
+    this.isLoading.set(true);
+    this.isError.set(null);
+
+    this.countryService.searchByCapital(query).subscribe((countries) => {
+      this.isLoading.set(false);
+      this.countries.set(countries);
+
+      console.log(countries);
+    });
   }
 }
